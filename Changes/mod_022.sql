22ab854e99cc70bc23658c75a2ef9c33faac022f
eloi=ribeiro <mail@eloiribeiro.eu>
2024-12-12 15:29:42 +0100

    rm comm

--- Changes/mod_022.sql
+++ Changes/mod_022.sql
@@ -36,33 +36,3 @@ CREATE TRIGGER trg_check_result_value
     FOR EACH ROW
     EXECUTE FUNCTION core.check_result_value();
 COMMENT ON TRIGGER trg_check_result_value ON core.result_phys_chem IS 'Verifies if the value assigned to the result is valid. See the function core.ceck_result_value function for implementation.';
-
-
--- Unnecessary trigger, table result_value_specimen does not exist
--- CREATE OR REPLACE FUNCTION core.check_result_value_specimen()
---     RETURNS trigger
---     LANGUAGE 'plpgsql'
---     COST 100
---     VOLATILE NOT LEAKPROOF
--- AS $BODY$
--- DECLARE
---     observation core.observation_numerical_specimen%ROWTYPE;
--- BEGIN
---     SELECT *
---       INTO observation
---       FROM core.observation_numerical_specimen
---      WHERE observation_numerical_specimen_id = NEW.observation_numerical_specimen_id;
-
---     IF NEW.value < observation.value_min OR NEW.value > observation.value_max THEN
---         RAISE EXCEPTION 'Result value outside admissable bounds for the related observation.';
---     ELSE
---         RETURN NEW;
---     END IF;
--- END;
--- $BODY$;
-
--- ALTER FUNCTION core.check_result_value_specimen()
---     OWNER TO glosis;
-
--- COMMENT ON FUNCTION core.check_result_value_specimen()
---     IS 'Checks if the value assigned to a result record is within the numerical bounds declared in the related observation (fields value_min and value_max).';
